bcebd6eab0f14400ba4091155340c52d0f5aeaf5
brandaorichard <rih.brandao@gmail.com>
2023-11-15 08:58:25 -0300

    Material update

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+// Cria porta do servidor e configura rotas
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  console.log('Requisicao recebida');
+  res.status(200).send([{ obj: 1}, { obj: 2 }])
+})
+
+app.get('/xablau1', (req, res) => {
+  res.status(200).send(['xablau-monocentrico', 'xablau monogamico'])
+}) 
+
+app.listen(3005)
+
+console.log('xablau');
